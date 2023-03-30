4b78b8ce266bd588ba6a4dd2c1dc883600f4f911
anjanikmaurya <89033899+anjanikmaurya@users.noreply.github.com>
2023-03-29 18:21:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,23 @@
 const tabLinks = document.querySelectorAll('nav ul li a');
 const tabContentSections = document.querySelectorAll('main section');
 
-// Hide all the tab content sections except the first one
-tabContentSections.forEach((section, index) => {
-  if (index !== 0) {
-    section.style.display = 'none';
-  }
-});
+// Check if this is the first time the page is loaded
+const isFirstTime = localStorage.getItem('isFirstTime') === null;
+if (isFirstTime) {
+  // If this is the first time, set the "About" tab as active
+  tabLinks[1].classList.add('active');
+  tabContentSections[0].style.display = 'none';
+  tabContentSections[1].style.display = 'block';
+  // Set the flag to indicate that the page has been loaded before
+  localStorage.setItem('isFirstTime', false);
+} else {
+  // If this is not the first time, hide all the tab content sections except the first one
+  tabContentSections.forEach((section, index) => {
+    if (index !== 0) {
+      section.style.display = 'none';
+    }
+  });
+}
 
 // Loop through all the tab links and add click event listeners
 tabLinks.forEach((link, index) => {
@@ -37,7 +48,7 @@ tabLinks.forEach((link, index) => {
   if (index !== 0) {
     const tooltip = document.createElement('div');
     tooltip.classList.add('tooltip');
- //   tooltip.textContent = 'Click to learn more';
+    //   tooltip.textContent = 'Click to learn more';
     link.appendChild(tooltip);
   }
 });
