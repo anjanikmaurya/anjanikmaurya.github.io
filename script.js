347972c94b79e92556885c64b7a22cd9cc0f2640
anjanikmaurya <89033899+anjanikmaurya@users.noreply.github.com>
2023-03-29 18:08:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-// Define the tab links and tab content sections
-const tabLinks = document.querySelectorAll('.nav__item-link');
-let tabContentSections;
+// Get all the tab links and tab content sections
+const tabLinks = document.querySelectorAll('nav ul li a');
+const tabContentSections = document.querySelectorAll('main section');
 
-// Wait for the page to load before showing the first tab content section
-window.addEventListener('load', () => {
-  tabContentSections = document.querySelectorAll('.tab__content');
-
-  // Show only the first tab content section
-  tabContentSections.forEach((section, index) => {
-    if (index === 0) {
-      section.classList.remove('hidden');
-    } else {
-      section.classList.add('hidden');
-    }
-  });
-});
-
-// Add click event listeners to the tab links
-tabLinks.forEach((link, index) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-
+// Loop through all the tab links and add click event listeners
+tabLinks.forEach(link => {
+  link.addEventListener('click', () => {
     // Remove the "active" class from all the tab links
-    tabLinks.forEach((tabLink) => {
+    tabLinks.forEach(tabLink => {
       tabLink.classList.remove('active');
     });
 
@@ -30,23 +14,13 @@ tabLinks.forEach((link, index) => {
     link.classList.add('active');
 
     // Hide all the tab content sections
-    tabContentSections.forEach((tabContent) => {
-      tabContent.classList.add('hidden');
+    tabContentSections.forEach(tabContent => {
+      tabContent.style.display = 'none';
     });
 
     // Show the content of the selected tab section
     const selectedTabContentId = link.getAttribute('href');
     const selectedTabContent = document.querySelector(selectedTabContentId);
-    selectedTabContent.classList.remove('hidden');
+    selectedTabContent.style.display = 'block';
   });
-
-  // If this is not the first tab link, add a tooltip to prompt the user to click it
-  /*
-  if (index !== 0) {
-    const tooltip = document.createElement('div');
-    tooltip.classList.add('tooltip');
-    tooltip.textContent = 'Click to learn more';
-    link.appendChild(tooltip);
-  }
-  */
 });
