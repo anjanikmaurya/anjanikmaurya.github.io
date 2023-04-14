64f9b4e723803fbf5d13af65e3bd037d83a5d34b
anjanikmaurya <89033899+anjanikmaurya@users.noreply.github.com>
2023-04-14 03:27:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ tabLinks.forEach((link, index) => {
 
 
 // Get the height of the footer
-const footerHeight = document.querySelector('footer').offsetHeight;
+//const footerHeight = document.querySelector('footer').offsetHeight;
 
 // Set the padding-bottom value of the main container to match the height of the footer
-document.querySelector('.main-container').style.paddingBottom = footerHeight + 'px';
+//document.querySelector('.main-container').style.paddingBottom = footerHeight + 'px';
